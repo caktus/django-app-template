3781f3b0a14b49c14bc7e0bf11886a820a3ab502
Mark Lavin <mlavin@caktusgroup.com>
2012-10-15 16:57:20 -0400

    Making the default test case fail.

--- app_name/tests.py
+++ app_name/tests.py
@@ -1,16 +1,12 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
+"Replace this with more appropriate tests for your application."
 
 from django.test import TestCase
 
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
+
+    def test_developer_is_lazy(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests that developer has written any tests.
         """
-        self.assertEqual(1 + 1, 2)
+        self.fail('You need to write some tests.')
