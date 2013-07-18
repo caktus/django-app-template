6924917e5b5d62d330c8f21774d1871e4fd4e746
Mark Lavin <mlavin@caktusgroup.com>
2013-07-18 16:13:32 -0400

    Include Python 3.2 and 3.3 classifiers.

--- setup.py
+++ setup.py
@@ -29,6 +29,8 @@ setup(
         'Programming Language :: Python',      
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
         'Framework :: Django',
         'Development Status :: 4 - Beta',
         'Operating System :: OS Independent',
